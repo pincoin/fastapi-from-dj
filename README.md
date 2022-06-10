a7877d07d898ce2c8af3952d1b07660a625420b1
jonghwa <jonghwa@pincoin.co.kr>
2022-06-10 22:55:33 +0900

    select queries

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # fastapi-from-dj
-This is a fastapi boilerplate template for quick start.
+This is a FastAPI boilerplate template for quick start.
 
 This project watches the same database schemes installed by Django.
 
@@ -7,18 +7,41 @@ This project is based on SQLAlchemy 1.4+ in order to utilize async connections.
 
 ## Features
 * SQLAlchemy Async Core + CRUD on PostgreSQL
-* OAuth2 + JWT (Django-compatible)
+* OAuth2 + JWT (Django scheme-compatible)
 
 # Getting started
 ## Requirements
 * fastapi
 * SQLAlchemy[asyncio]
+* asyncpg
+* pydantic[dotenv]
 * uvicorn
-* asyncpg (PostgreSQL)
 * alembic
 * uvicorn
 
-## app/conf/local.env
+## Settings
+`app/local.env`
+
+```py
+app_name="FastAPI from Django"
+debug=True
+uvicorn_reload=True
+secret_key="Minimum-length-32-secret-key-string"
+sqlalchemy_database_uri="postgresql+asyncpg://username:password@host:port/database"
+```
+
+You can create the `app/production.env` file for production server, and all `.env` files are hidden by `.gitignore`.
+
+## Run
+```
+$ python app/main.py
+```
+
+SQLAlchemy 2.0 migration must be considered for consistent development, so you may enable all of SQLAlchemy warings as follows:
+
+```
+$ SQLALCHEMY_WARN_20=1 python -W always::DeprecationWarning app/main.py
+```
 
 # Package structure
 ```
@@ -38,3 +61,24 @@ package/
 * `responses.py`: Reusable HTTP responses
 * `exceptions.py`: Reusable HTTP exceptions
 * `utils.py`: Helpers
+
+
+# Django or FastAPI
+I decided to run Django and FastAPI servers because of the following reasons.
+
+## Django
+Pros
+* All-in-one framework with a rich of 3rd party libraries in django-ecosystem
+* Django ORM and Admin page
+Cons
+* REST API is supported by DRF(Django Rest Framework)
+* WSGI
+* Websocket not supported
+
+## FastAPI
+Pros
+* Async/ASGI
+* Websocket
+Cons
+* Session not supported
+* Samll communities with small 3rd party libraries
--- app/auth/routers.py
+++ app/auth/routers.py
@@ -1,3 +1,4 @@
+from math import perm
 from conf.config import get_settings
 from conf.dependencies import engine_begin, engine_connect
 from fastapi import APIRouter, Depends
@@ -87,14 +88,32 @@ async def list_permissions_of_user(conn: AsyncConnection = Depends(engine_connec
 
 
 @router.get("/content-types")
-async def list_content_types(conn: AsyncConnection = Depends(engine_connect)):
-    return {}
+async def list_content_types(
+    skip: int = 0,
+    take: int = 100,
+    conn: AsyncConnection = Depends(engine_connect),
+):
+    cr: CursorResult = await conn.execute(
+        models.content_types.select().offset(skip).limit(take)
+    )
+    return cr.fetchall()
 
 
 @router.get("/content_types/{content_type_id}")
-async def get_content_type(conn: AsyncConnection = Depends(engine_connect)):
-    return {}
+async def get_content_type(
+    content_type_id: int,
+    conn: AsyncConnection = Depends(engine_connect),
+):
+    cr: CursorResult = await conn.execute(
+        models.content_types.select().where(models.content_types.c.id == content_type_id)
+    )
 
+    content_type = cr.first()
+
+    if content_type:
+        return content_type
+
+    raise exceptions.content_type_not_found_exception()
 
 @router.post("/content-types/")
 async def create_content_type(conn: AsyncConnection = Depends(engine_begin)):
@@ -119,13 +138,31 @@ async def list_permissions_of_content_type(
 
 
 @router.get("/groups")
-async def list_groups(conn: AsyncConnection = Depends(engine_connect)):
-    return {}
-
+async def list_groups(
+    skip: int = 0,
+    take: int = 100,
+    conn: AsyncConnection = Depends(engine_connect),
+):
+    cr: CursorResult = await conn.execute(
+        models.groups.select().offset(skip).limit(take)
+    )
+    return cr.fetchall()
 
 @router.get("/groups/{group_id}")
-async def get_group(conn: AsyncConnection = Depends(engine_connect)):
-    return {}
+async def get_group(
+    group_id: int,
+    conn: AsyncConnection = Depends(engine_connect),
+):
+    cr: CursorResult = await conn.execute(
+        models.groups.select().where(models.groups.c.id == group_id)
+    )
+
+    group = cr.first()
+
+    if group:
+        return group
+
+    raise exceptions.content_type_not_found_exception()
 
 
 @router.post("/groups/")
@@ -159,38 +196,32 @@ async def delete_user_of_group(conn: AsyncConnection = Depends(engine_connect)):
 
 
 @router.get("/permissions")
-async def list_permissions(conn: AsyncConnection = Depends(engine_connect)):
-    return {}
+async def list_permissions(skip: int = 0,
+    take: int = 100,
+    conn: AsyncConnection = Depends(engine_connect),
+):
+    cr: CursorResult = await conn.execute(
+        models.permissions.select().offset(skip).limit(take)
+    )
+    return cr.fetchall()
 
 
 @router.get("/permissions/{permission_id}")
-async def get_permission(conn: AsyncConnection = Depends(engine_connect)):
-    return {}
+async def get_permission(
+    permission_id: int,
+    conn: AsyncConnection = Depends(engine_connect),
+):
+    cr: CursorResult = await conn.execute(
+        models.permissions.select().where(models.permissions.c.id == permission_id)
+    )
 
+    permission = cr.first()
 
-@router.post("/permissions/")
-async def create_permission(conn: AsyncConnection = Depends(engine_begin)):
-    return {}
+    if permission:
+        return permission
 
+    raise exceptions.permission_not_found_exception()
 
-@router.put("/permissions/{permission_id}")
-async def update_permission(conn: AsyncConnection = Depends(engine_begin)):
-    return {}
-
-
-@router.delete("/permissions/{permission_id}")
-async def delete_permission(conn: AsyncConnection = Depends(engine_begin)):
-    return {}
-
-
-@router.get("/permissions")
-async def list_permissions(conn: AsyncConnection = Depends(engine_connect)):
-    return {}
-
-
-@router.get("/permissions/{permission_id}")
-async def get_permission(conn: AsyncConnection = Depends(engine_connect)):
-    return {}
 
 
 @router.post("/permissions/")
@@ -246,8 +277,17 @@ async def list_content_types_of_permission(
 
 
 @router.get("/superusers")
-async def list_superusers(conn: AsyncConnection = Depends(engine_connect)):
-    return {}
+async def list_superusers(skip: int = 0,
+    take: int = 100,
+    conn: AsyncConnection = Depends(engine_connect),
+):
+    cr: CursorResult = await conn.execute(
+        models.users.select().where(models.users.c.is_superuser==True) \
+            .offset(skip)\
+            .limit(take)
+    )
+    return cr.fetchall()
+
 
 
 @router.post("/superusers")
