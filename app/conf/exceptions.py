f369adcbdbe6a501f39b977b57ac4b182e7d789a
jonghwa <jonghwa@pincoin.co.kr>
2022-06-13 13:16:28 +0900

    401, 403 exceptions

--- app/conf/exceptions.py
+++ app/conf/exceptions.py
@@ -2,12 +2,20 @@ from fastapi import HTTPException
 
 
 def bad_request_exception():
-    return HTTPException(status_code=400, detail="Bad request")
+    return HTTPException(status_code=400, detail="Bad Request")
+
+
+def unauthorized_exception():
+    return HTTPException(status_code=401, detail="Invalid Authentication")
+
+
+def forbidden_exception():
+    return HTTPException(status_code=403, detail="Forbidden")
 
 
 def item_not_found_exception(item):
-    return HTTPException(status_code=404, detail=f"{item} not found")
+    return HTTPException(status_code=404, detail=f"{item} Not Found")
 
 
 def conflict_exception():
-    return HTTPException(status_code=409, detail="Integrity conflict")
+    return HTTPException(status_code=409, detail="Integrity error")
