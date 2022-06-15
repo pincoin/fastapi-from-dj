282e4c30afeadb1b943b9925159ea09bd3b31f77
jonghwa <dev@pincoin.co.kr>
2022-06-15 15:24:46 +0900

    alembic file template path

--- alembic/versions/2022_06_15_1512-a522b478adde_auth_init.py
+++ alembic/versions/2022_06_15_1512-a522b478adde_auth_init.py
@@ -1,15 +1,15 @@
-"""init
+"""auth init
 
-Revision ID: ef6fbe3476c9
+Revision ID: a522b478adde
 Revises: 
-Create Date: 2022-06-14 23:49:40.028003
+Create Date: 2022-06-15 15:12:49.946793
 
 """
 import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = "ef6fbe3476c9"
+revision = "a522b478adde"
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -88,7 +88,7 @@ def upgrade() -> None:
         ),
     )
 
-    op.create_table(
+    django_content_type = op.create_table(
         "django_content_type",
         sa.Column(
             "id",
@@ -107,7 +107,7 @@ def upgrade() -> None:
         sa.UniqueConstraint("app_label", "model"),
     )
 
-    op.create_table(
+    auth_permission = op.create_table(
         "auth_permission",
         sa.Column(
             "id",
@@ -338,6 +338,162 @@ def upgrade() -> None:
             "applied",
             sa.types.TIMESTAMP(timezone=True),
         ),
+    )
+
+    op.bulk_insert(
+        django_content_type,
+        [
+            {
+                "app_label": "admin",
+                "model": "logentry",
+            },
+            {
+                "app_label": "auth",
+                "model": "permission",
+            },
+            {
+                "app_label": "auth",
+                "model": "group",
+            },
+            {
+                "app_label": "auth",
+                "model": "user",
+            },
+            {
+                "app_label": "contenttypes",
+                "model": "contenttype",
+            },
+            {
+                "app_label": "sessions",
+                "model": "session",
+            },
+        ],
+    )
+
+    op.bulk_insert(
+        auth_permission,
+        [
+            {
+                "name": "Can add log entry",
+                "content_type_id": 1,
+                "codename": "add_logentry",
+            },
+            {
+                "name": "Can change log entry",
+                "content_type_id": 1,
+                "codename": "change_logentry",
+            },
+            {
+                "name": "Can delete log entry",
+                "content_type_id": 1,
+                "codename": "delete_logentry",
+            },
+            {
+                "name": "Can view log entry",
+                "content_type_id": 1,
+                "codename": "view_logentry",
+            },
+            {
+                "name": "Can add permission",
+                "content_type_id": 2,
+                "codename": "add_permission",
+            },
+            {
+                "name": "Can change permission",
+                "content_type_id": 2,
+                "codename": "change_permission",
+            },
+            {
+                "name": "Can delete permission",
+                "content_type_id": 2,
+                "codename": "delete_permission",
+            },
+            {
+                "name": "Can view permission",
+                "content_type_id": 2,
+                "codename": "view_permission",
+            },
+            {
+                "name": "Can add group",
+                "content_type_id": 3,
+                "codename": "add_group",
+            },
+            {
+                "name": "Can change group",
+                "content_type_id": 3,
+                "codename": "change_group",
+            },
+            {
+                "name": "Can delete group",
+                "content_type_id": 3,
+                "codename": "delete_group",
+            },
+            {
+                "name": "Can view group",
+                "content_type_id": 3,
+                "codename": "view_group",
+            },
+            {
+                "name": "Can add user",
+                "content_type_id": 4,
+                "codename": "add_user",
+            },
+            {
+                "name": "Can change user",
+                "content_type_id": 4,
+                "codename": "change_user",
+            },
+            {
+                "name": "Can delete user",
+                "content_type_id": 4,
+                "codename": "delete_user",
+            },
+            {
+                "name": "Can view user",
+                "content_type_id": 4,
+                "codename": "view_user",
+            },
+            {
+                "name": "Can add content type",
+                "content_type_id": 5,
+                "codename": "add_contenttype",
+            },
+            {
+                "name": "Can change content type",
+                "content_type_id": 5,
+                "codename": "change_contenttype",
+            },
+            {
+                "name": "Can delete content type",
+                "content_type_id": 5,
+                "codename": "delete_contenttype",
+            },
+            {
+                "name": "Can view content type",
+                "content_type_id": 5,
+                "codename": "view_contenttype",
+            },
+            {
+                "name": "Can add session",
+                "content_type_id": 6,
+                "codename": "add_session",
+            },
+            {
+                "name": "Can change session",
+                "content_type_id": 6,
+                "codename": "change_session",
+            },
+            {
+                "name": "Can delete session",
+                "content_type_id": 6,
+                "codename": "delete_session",
+            },
+            {
+                "name": "Can view session",
+                "content_type_id": 6,
+                "codename": "view_session",
+            },
+        ],
     )
 
 
