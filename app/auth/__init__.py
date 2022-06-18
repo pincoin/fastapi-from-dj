7b0e046c86e271ac8e6218a386a2487e6770136e
jonghwa <dev@pincoin.co.kr>
2022-06-18 21:44:14 +0900

    delete dependencies

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,6 +1,5 @@
-from . import dependencies as dependencies
+from . import backends as backends
 from . import hashers as hashers
 from . import models as models
 from . import routers as routers
 from . import schemas as schemas
-from . import backends as backends
