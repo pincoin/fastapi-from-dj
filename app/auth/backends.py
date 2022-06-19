a17241bdf584a0518546a69056a03cd4892c716c
jonghwa <dev@pincoin.co.kr>
2022-06-19 18:02:08 +0900

    get_current_user dependency in backends

--- app/auth/backends.py
+++ app/auth/backends.py
@@ -44,42 +44,6 @@ class BaseAuthenticationBackend(metaclass=abc.ABCMeta):
 
 
 class AuthenticationBackend(BaseAuthenticationBackend):
-    async def get_current_user(
-        self,
-        token: str = fastapi.Depends(oauth2_scheme),
-    ) -> dict:
-        try:
-            payload = jwt.decode(
-                token,
-                settings.secret_key,
-                algorithms=[settings.jwt_algorithm],
-            )
-
-            username: str = payload.get("sub")
-            user_id: int = payload.get("id")
-
-            if username is None or user_id is None:
-                raise exceptions.invalid_token_exception()
-
-            return {"username": username, "id": user_id}
-        except JWTError:
-            raise exceptions.invalid_token_exception()
-
-    async def get_superuser(
-        self,
-        current_user: dict = fastapi.Depends(get_current_user),
-        conn: sa.ext.asyncio.engine.AsyncConnection = fastapi.Depends(engine_connect),
-    ) -> dict:
-        stmt = sa.select(models.users).where(
-            models.users.c.id == current_user["id"],
-            models.users.c.is_active == True,
-            models.users.c.is_superuser == True,
-        )
-
-        row = await CRUDModel(conn).get_one(stmt)
-
-        return row._mapping if row else None
-
     async def authenticate(
         self,
         username: str,
@@ -311,6 +275,40 @@ class AuthenticationBackend(BaseAuthenticationBackend):
             stmt = stmt.where(models.users.c.is_superuser == False)
 
         return await CRUDModel(conn).get_all(stmt)
+
+
+async def get_current_user(token: str = fastapi.Depends(oauth2_scheme)) -> dict:
+    try:
+        payload = jwt.decode(
+            token,
+            settings.secret_key,
+            algorithms=[settings.jwt_algorithm],
+        )
+
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+
+        if username is None or user_id is None:
+            raise exceptions.invalid_token_exception()
+
+        return {"username": username, "id": user_id}
+    except JWTError:
+        raise exceptions.invalid_token_exception()
+
+
+async def get_superuser(
+    current_user: dict = fastapi.Depends(get_current_user),
+    conn: sa.ext.asyncio.engine.AsyncConnection = fastapi.Depends(engine_connect),
+) -> dict:
+    stmt = sa.select(models.users).where(
+        models.users.c.id == current_user["id"],
+        models.users.c.is_active == True,
+        models.users.c.is_superuser == True,
+    )
+
+    row = await CRUDModel(conn).get_one(stmt)
+
+    return row._mapping if row else None
 
 
 @lru_cache(maxsize=1)
